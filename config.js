8769c25abf0279b2d35cd9e864ed22dcd7c125c5
Fabricio Samanez <fabricio.samanez@gmail.com>
2021-06-09 09:50:46 -0500

    variables de entorno finales

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://user:1234@cluster0.k7bnf.mongodb.net/telegram?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL,
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/app',
